6b0136e6337247aab4374e25fe5a43f2873b1505
Victor Mello <victorsmello93@gmail.com>
2023-10-03 08:28:24 -0300

    fix: knexjs pg connection

--- src/app/exemplo/adaptadores/db/knex/knexfile.js
+++ src/app/exemplo/adaptadores/db/knex/knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     host: "127.0.0.1",
     port: 5432,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: String(process.env.DB_PASSWORD),
     database: process.env.DB_NAME
   },
   migrations: {
